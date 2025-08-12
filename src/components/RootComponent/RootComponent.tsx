c13e447f4d7edb2fd391f07c8dd63f883096c52e
Marshall Hunt <34864792+MarshTheBacca@users.noreply.github.com>
2025-08-12 21:49:57 +0100

    Initial commit

---
+++ src/components/RootComponent/RootComponent.tsx
@@ -0,0 +1,8 @@
+export function RootComponent(): React.ReactElement<HTMLDivElement> {
+  return (
+    <div>
+      <h1>This is the Root Component</h1>
+      <p>Build your webapp</p>
+    </div>
+  )
+}
