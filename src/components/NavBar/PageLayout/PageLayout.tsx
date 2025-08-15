defb972b3559e02e75fd459db9736372f9426157
MarshTheBacca <marshallhunt@live.com>
2025-08-15 21:26:19 +0100

    Add `PageLayout` wrapper for animating between pages

---
+++ src/components/NavBar/PageLayout/PageLayout.tsx
@@ -0,0 +1,18 @@
+import { motion } from 'framer-motion'
+export function PageLayout({
+  children,
+}: {
+  children: React.ReactNode
+}): React.ReactElement {
+  return (
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
+      transition={{ duration: 0.25 }}
+      className={'flex-grow flex-col items-center bg-gray-800 font-bold'}
+    >
+      {children}
+    </motion.div>
+  )
+}
